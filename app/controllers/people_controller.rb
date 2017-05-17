cf158895d756954a7bc3b00e0ff9e81e5d1845ea
Aaron Poncé <aaron.m.ponce@gmail.com>
2017-05-17 16:57:34 +0800

    enable sort on column header

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,9 +7,9 @@ class PeopleController < ApplicationController
   def index
 
     @people = Person.all
-                     .search(person_params[:search])
+                     .search(params[:search])
                      .order(sort_column + " " + sort_direction)
-                     .paginate(:per_page => 3, :page => person_params[:page])
+                     .paginate(:per_page => 5, :page => params[:page])
   end
 
   def import
@@ -23,11 +23,11 @@ class PeopleController < ApplicationController
   private
 
   def sort_column
-    Person.column_names.include?(person_params[:sort]) ? person_params[:sort] : "lastname"
+    Person.column_names.include?(params[:sort]) ? params[:sort] : "lastname"
   end
 
   def sort_direction
-    %w[asc desc].include?(person_params[:direction]) ? person_params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
   # allow the white list.
